309014c5f79ac20821ba531552b5e82a183bc37f
zhangwei_modooplay <wei.zhang@modooplay.com>
2020-12-30 14:07:48 +0800

    new table

---
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/Tables/Generate/Gameplay/Visitor/TDVisitorConfig.cs
@@ -0,0 +1,78 @@
+//Auto Generate Don't Edit it
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Qarth;
+
+namespace GameWish.Game
+{
+    public partial class TDVisitorConfig
+    {
+        
+       
+        private EInt m_Id = 0;   
+        private string m_Name;   
+        private string m_AnimationName;  
+        
+        //private Dictionary<string, TDUniversally.FieldData> m_DataCacheNoGenerate = new Dictionary<string, TDUniversally.FieldData>();
+      
+        /// <summary>
+        /// ID
+        /// </summary>
+        public  int  id {get { return m_Id; } }
+       
+        /// <summary>
+        /// 访客名
+        /// </summary>
+        public  string  name {get { return m_Name; } }
+       
+        /// <summary>
+        /// 访客文本
+        /// </summary>
+        public  string  animationName {get { return m_AnimationName; } }
+       
+
+        public void ReadRow(DataStreamReader dataR, int[] filedIndex)
+        {
+          //var schemeNames = dataR.GetSchemeName();
+          int col = 0;
+          while(true)
+          {
+            col = dataR.MoreFieldOnRow();
+            if (col == -1)
+            {
+              break;
+            }
+            switch (filedIndex[col])
+            { 
+            
+                case 0:
+                    m_Id = dataR.ReadInt();
+                    break;
+                case 1:
+                    m_Name = dataR.ReadString();
+                    break;
+                case 2:
+                    m_AnimationName = dataR.ReadString();
+                    break;
+                default:
+                    //TableHelper.CacheNewField(dataR, schemeNames[col], m_DataCacheNoGenerate);
+                    break;
+            }
+          }
+
+        }
+        
+        public static Dictionary<string, int> GetFieldHeadIndex()
+        {
+          Dictionary<string, int> ret = new Dictionary<string, int>(3);
+          
+          ret.Add("Id", 0);
+          ret.Add("Name", 1);
+          ret.Add("AnimationName", 2);
+          return ret;
+        }
+    } 
+}//namespace LR
