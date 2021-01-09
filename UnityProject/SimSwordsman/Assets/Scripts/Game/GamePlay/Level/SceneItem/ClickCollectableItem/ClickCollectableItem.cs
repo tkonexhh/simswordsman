18c5b818b4c9bc45e7f1a572cf54b83bfc664092
UnityZhang <1198889547@qq.com>
2021-01-09 11:20:08 +0800

    11

---
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Level/SceneItem/ClickCollectableItem/ClickCollectableItem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameWish.Game
+{
+	public class ClickCollectableItem : MonoBehaviour
+	{
+        public RawMaterial materialType;
+
+        public void OnClicked()
+        {
+            //MainGameMgr.S.InventoryMgr.AddItem()
+        }
+	}
+	
+}
