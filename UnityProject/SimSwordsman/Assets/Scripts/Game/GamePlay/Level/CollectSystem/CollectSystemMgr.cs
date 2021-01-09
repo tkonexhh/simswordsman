4778a99d9461cf90c0992a3f5a87c83611f1d984
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-01-09 11:11:37 +0800

    collectsystem

---
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Level/CollectSystem/CollectSystemMgr.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameWish.Game
+{
+	public class CollectSystemMgr : MonoBehaviour, IMgr
+    {
+        #region IMgr
+        public void OnInit()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnDestroyed()
+        {
+
+        }
+
+        #endregion
+    }
+
+}
