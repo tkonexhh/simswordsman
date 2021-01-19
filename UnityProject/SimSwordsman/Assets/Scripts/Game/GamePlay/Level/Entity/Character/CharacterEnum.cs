8a5f6d8c365333aab60a5aa13420a97c03943428
Lvmm <mengmeng.lv@modooplay.com>
2021-01-19 10:30:21 +0800

    弟子界面UI替换

--- UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Level/Entity/Character/CharacterEnum.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Level/Entity/Character/CharacterEnum.cs
@@ -7,8 +7,17 @@ namespace GameWish.Game
 {
 	public enum CharacterQuality
 	{
+        /// <summary>
+        /// 平民级
+        /// </summary>
         Normal = 1,
+        /// <summary>
+        /// 精英级
+        /// </summary>
         Good = 2,
+        /// <summary>
+        /// 天才级
+        /// </summary>
         Perfect = 3,
         //Excellent = 4,
 	}
