99a599bb797af37c2b0a234443595b24223aa165
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-01-22 18:40:48 +0800

    task collect res bug fix

--- UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Level/Entity/Character/StateMachine/CharacterState/CharacterStateCollectRes.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Level/Entity/Character/StateMachine/CharacterState/CharacterStateCollectRes.cs
@@ -93,11 +93,19 @@ namespace GameWish.Game
 
             m_ReachTargetPos = true;
 
-            string animName = GetCollectResAnim();
-            m_Controller.CharacterView.PlayAnim(animName, true, null);
-            m_Controller.FaceTo(m_TaskCollectableItem.transform.position.x);
+            m_TaskCollectableItem = MainGameMgr.S.CommonTaskMgr.GetTaskCollectableItem(m_CollectedObjType);
+            if (m_TaskCollectableItem != null)
+            {
+                string animName = GetCollectResAnim();
+                m_Controller.CharacterView.PlayAnim(animName, true, null);
+                m_Controller.FaceTo(m_TaskCollectableItem.transform.position.x);
 
-            m_TaskCollectableItem?.OnStartCollected(m_Controller.GetPosition());
+                m_TaskCollectableItem?.OnStartCollected(m_Controller.GetPosition());
+            }
+            else
+            {
+                m_Controller.SetState(CharacterStateID.Wander);
+            }
         }
 
         private string GetCollectResAnim()
