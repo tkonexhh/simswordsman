6c9a9666d0c1ab547658dc9994a7001a44a11955
Lvmm <mengmeng.lv@modooplay.com>
2021-01-22 14:53:34 +0800

    焦点丢失  倒计时停止问题   建造界面字体调整

--- UnityProject/SimSwordsman/Assets/Scripts/Game/UIScripts/Common/CountDownItem.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/UIScripts/Common/CountDownItem.cs
@@ -28,6 +28,8 @@ namespace GameWish.Game
         /// </summary>
         public Action OnCountDownOverEvent;
 
+        private string startTime;
+
         public CountDownItem() { }
         public CountDownItem(string _name,int tickCount,int interval = 1)
         {
@@ -54,6 +56,15 @@ namespace GameWish.Game
         public void IncreasTickTime(int delta)
         {
             m_TickCount += delta;
+        }
+        public void ReduceTickTime(int delta)
+        {
+            m_TickCount -= delta;
+            if (m_TickCount<=0)
+            {
+                OnCountDownOverEvent?.Invoke();
+                TimeUpdateMgr.S.AddFinishedObservers(this);
+            }
         }
 
         public string GetID()
@@ -110,7 +121,7 @@ namespace GameWish.Game
 
         public void OnFinished()
         {
-            //倒计时结束调用
+            EventSystem.S.UnRegister(EngineEventID.OnAfterApplicationFocusChange, HandleAddListenerEvent);
         }
 
         public void OnPause()
@@ -123,11 +134,38 @@ namespace GameWish.Game
 
         public void OnStart()
         {
-            Debug.Log(m_CountDownID+"倒计时开始了");
+            Log.i("CoungDown is start!");
+            EventSystem.S.Register(EngineEventID.OnAfterApplicationFocusChange, HandleAddListenerEvent);
+        }
+        private void HandleAddListenerEvent(int key, object[] param)
+        {
+
+            if ((bool)param[0])
+            {
+                Debug.LogError("切换到前台时执行");
+                ReduceTickTime(ComputingTime(startTime));
+            }
+            else
+            {
+                //切换到后台时执行
+                Debug.LogError("切换到后台时执行");
+                startTime = DateTime.Now.ToString();
+                // RefreshPracticeFieldState();
+
+            }
         }
 
-       
-
+        private int ComputingTime(string time)
+        {
+            DateTime dateTime;
+            DateTime.TryParse(time, out dateTime);
+            if (dateTime != null)
+            {
+                TimeSpan timeSpan = new TimeSpan(DateTime.Now.Ticks) - new TimeSpan(dateTime.Ticks);
+                return (int)timeSpan.TotalSeconds;
+            }
+            return 0;
+        }
         public bool ShouldRemoveWhenMapChanged()
         {
             return true;
