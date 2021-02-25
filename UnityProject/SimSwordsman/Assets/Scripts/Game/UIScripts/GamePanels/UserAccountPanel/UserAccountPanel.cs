af234834ab30176a88240183f733f433c954f447
will.yi <will.yi@PC202003270018>
2021-02-25 16:27:36 +0800

    用户账号面板 添加UI

--- UnityProject/SimSwordsman/Assets/Scripts/Game/UIScripts/GamePanels/UserAccountPanel/UserAccountPanel.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/UIScripts/GamePanels/UserAccountPanel/UserAccountPanel.cs
@@ -26,6 +26,21 @@ namespace GameWish.Game
 		[SerializeField]
 		private Button m_PrivateBtn;
 
+        [SerializeField]
+        private Button m_MusicBtn;
+        [SerializeField]
+        private GameObject m_MusicOffObj;
+        [SerializeField]
+        private GameObject m_MusicOnObj;
+        [SerializeField]
+        private Button m_SoundBtn;
+        [SerializeField]
+        private GameObject m_SoundOffObj;
+        [SerializeField]
+        private GameObject m_SoundOnObj;
+        private bool isMuiscOn;
+        private bool isSoundOn;
+
         protected override void OnUIInit()
         {
             base.OnUIInit();
@@ -33,11 +48,33 @@ namespace GameWish.Game
 			m_CloseBtn.onClick.AddListener(OnCloseBtnClickCallBack);
             m_LogOutBtn.onClick.AddListener(OnLogOutBtnClickCallBack);
             m_PrivateBtn.onClick.AddListener(OnPrivateBtnClickCallBack);
+
+            m_MusicBtn.onClick.AddListener(() => { UpdateMusic(!isMuiscOn); });
+            m_SoundBtn.onClick.AddListener(() => { UpdateSound(!isSoundOn); });
+        }
+        private void UpdateMusic(bool ison)
+        {
+            isMuiscOn = ison;
+            AudioMgr.S.isMusicEnable = ison;
+            m_MusicOffObj.SetActive(!ison);
+            m_MusicOnObj.SetActive(ison);
+        }
+        private void UpdateSound(bool ison)
+        {
+            isSoundOn = ison;
+            AudioMgr.S.isSoundEnable = ison;
+            m_SoundOffObj.SetActive(!ison);
+            m_SoundOnObj.SetActive(ison);
         }
         protected override void OnPanelOpen(params object[] args)
         {
             base.OnPanelOpen(args);
+
             OpenDependPanel(EngineUI.MaskPanel, -1, null);
+
+            UpdateMusic(AudioMgr.S.isMusicEnable);
+            UpdateSound(AudioMgr.S.isSoundEnable);
+
             UpdateUseAccountInfo();
         }
         protected override void OnPanelHideComplete()
