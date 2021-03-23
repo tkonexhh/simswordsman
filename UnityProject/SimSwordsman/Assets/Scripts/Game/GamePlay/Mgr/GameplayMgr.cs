3edd9a9e75a8cef310e0e020b70ff8cc52d072fd
will.yi <will.yi@PC202003270018>
2021-03-23 10:17:17 +0800

    send  split channel

--- UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Mgr/GameplayMgr.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/GamePlay/Mgr/GameplayMgr.cs
@@ -88,7 +88,24 @@ namespace GameWish.Game
 
             m_IsLoadingBarFinished = true;
 
+            SendSplitChannelData();
+
             yield return null;
+        }
+
+        private void SendSplitChannelData()
+        {
+            string channelName = CustomExtensions.GetSDKChannel();
+            Debug.LogError("split channel:" + channelName);
+#if UNITY_ANDROID
+            Dictionary<string, object> dic = new Dictionary<string, object>();
+            dic.Add("channel", channelName);
+            DataAnalysisMgr.S.CustomEventDic("split_channel", dic);
+#else
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("channel", channelName);
+            DataAnalysisMgr.S.CustomEventDic("split_channel", dic);
+#endif
         }
 
         private void OnGamePauseChange(int key, params object[] args)
