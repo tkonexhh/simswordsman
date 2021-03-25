1016bee035d6154d07aeab1080d9798354495bdd
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-03-25 10:00:35 +0800

    adscene

---
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/Tables/Generate/Sdk/TDAdSceneConfig.cs
@@ -0,0 +1,68 @@
+//Auto Generate Don't Edit it
+using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Qarth;
+
+namespace GameWish.Game
+{
+    public partial class TDAdSceneConfig
+    {
+        
+       
+        private string m_Id;   
+        private string m_SceneId;  
+        
+        //private Dictionary<string, TDUniversally.FieldData> m_DataCacheNoGenerate = new Dictionary<string, TDUniversally.FieldData>();
+      
+        /// <summary>
+        /// ID
+        /// </summary>
+        public  string  id {get { return m_Id; } }
+       
+        /// <summary>
+        /// 场景id
+        /// </summary>
+        public  string  sceneId {get { return m_SceneId; } }
+       
+
+        public void ReadRow(DataStreamReader dataR, int[] filedIndex)
+        {
+          //var schemeNames = dataR.GetSchemeName();
+          int col = 0;
+          while(true)
+          {
+            col = dataR.MoreFieldOnRow();
+            if (col == -1)
+            {
+              break;
+            }
+            switch (filedIndex[col])
+            { 
+            
+                case 0:
+                    m_Id = dataR.ReadString();
+                    break;
+                case 1:
+                    m_SceneId = dataR.ReadString();
+                    break;
+                default:
+                    //TableHelper.CacheNewField(dataR, schemeNames[col], m_DataCacheNoGenerate);
+                    break;
+            }
+          }
+
+        }
+        
+        public static Dictionary<string, int> GetFieldHeadIndex()
+        {
+          Dictionary<string, int> ret = new Dictionary<string, int>(2);
+          
+          ret.Add("Id", 0);
+          ret.Add("SceneId", 1);
+          return ret;
+        }
+    } 
+}//namespace LR
