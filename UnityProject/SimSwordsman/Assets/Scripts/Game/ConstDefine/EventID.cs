6e1b9288aab481cd2ce211df4efff993aa29f4a1
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-03-25 17:13:50 +0800

    unlockCommonTaskSystem

--- UnityProject/SimSwordsman/Assets/Scripts/Game/ConstDefine/EventID.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/ConstDefine/EventID.cs
@@ -63,6 +63,7 @@ namespace GameWish.Game
         OnUpgradeRefreshEvent,
 
         // MainTask
+        OnUnlockCommonTaskSystem,
         OnCommonTaskRefreshed,
         OnTaskManualFinished,
         //OnTaskFinished,
