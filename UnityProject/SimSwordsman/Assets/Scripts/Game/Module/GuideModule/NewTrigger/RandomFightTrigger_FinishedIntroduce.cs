7b6dbebc53d5fdc9c9e49bb7163e4b12e533806c
will.yi <will.yi@PC202003270018>
2021-03-26 10:04:31 +0800

    引导bug修改

--- UnityProject/SimSwordsman/Assets/Scripts/Game/Module/GuideModule/NewTrigger/RandomFightTrigger_FinishedIntroduce.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/Module/GuideModule/NewTrigger/RandomFightTrigger_FinishedIntroduce.cs
@@ -42,7 +42,7 @@ namespace GameWish.Game
             m_Listener = null;
             EventSystem.S.UnRegister(EventID.RandomFightTrigger_FinishedIntroduce, OnEventListener);
 
-            EventSystem.S.Send(EventID.BuildPracticeFieldEastTrigger);
+            EventSystem.S.Send(EventID.OnRecruitmentSystem_IntroduceTrigger1);
         }
     }	
 }
