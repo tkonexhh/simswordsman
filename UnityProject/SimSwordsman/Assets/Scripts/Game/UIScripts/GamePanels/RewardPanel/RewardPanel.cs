050e4b9e7abaab532b089045cc563626510fa71c
neo.xu <neo.xu@modooplay.com>
2021-03-31 20:04:07 +0800

    修改特效层级

--- UnityProject/SimSwordsman/Assets/Scripts/Game/UIScripts/GamePanels/RewardPanel/RewardPanel.cs
+++ UnityProject/SimSwordsman/Assets/Scripts/Game/UIScripts/GamePanels/RewardPanel/RewardPanel.cs
@@ -91,7 +91,7 @@ namespace GameWish.Game
                     m_Items.Add(item);
                 }
                 m_Items[i].gameObject.SetActive(true);
-                m_Items[i].Init(this, rewards[i], m_SortingOrder);
+                m_Items[i].Init(this, rewards[i], maxSortingOrder);
             }
 
 
